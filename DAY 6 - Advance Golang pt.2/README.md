3dc49e74043930c46ef416137a6930f8a375d045
Septianleonardo <dwigantoroleonardoseptian@gmail.com>
2022-10-31 21:20:15 +0700

    upload tugas advance golang pt 2

---
+++ DAY 6 - Advance Golang pt.2/README.md
@@ -0,0 +1,14 @@
+## Advance Golang pt.2
+Buat satu aplikasi CRUD dengan Golang tanpa DB - menggunakan Echo dan middleware
+
+### Membuat User Baru
+![buat-user](images/1.PNG)
+
+### Mendapatkan User
+![dapat-user](images/2.PNG)
+
+### Update User
+![update-user](images/3.PNG)
+
+### Menghapus User
+![hapus-user](images/4.PNG)
---
+++ DAY 6 - Advance Golang pt.2/server.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+	"sync"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+// type user
+type (
+	// struct user dengan field ID dan Name
+	user struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	}
+)
+
+// variabel global
+var (
+	users = map[int]*user{}
+	seq   = 1
+	lock  = sync.Mutex{}
+)
+
+// funsgi untuk membuat user
+func createUser(c echo.Context) error {
+	lock.Lock()
+	defer lock.Unlock()
+	u := &user{
+		ID: seq,
+	}
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	users[u.ID] = u
+	seq++
+	return c.JSON(http.StatusCreated, u)
+}
+
+// func getUser untuk mendapatkan user dengan id
+func getUser(c echo.Context) error {
+	lock.Lock()
+	defer lock.Unlock()
+	id, _ := strconv.Atoi(c.Param("id"))
+	return c.JSON(http.StatusOK, users[id])
+}
+
+// func updateUser untuk mengupdate user
+func updateUser(c echo.Context) error {
+	lock.Lock()
+	defer lock.Unlock()
+	u := new(user)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	id, _ := strconv.Atoi(c.Param("id"))
+	users[id].Name = u.Name
+	return c.JSON(http.StatusOK, users[id])
+}
+
+// fungsi delete user
+func deleteUser(c echo.Context) error {
+	lock.Lock()
+	defer lock.Unlock()
+	id, _ := strconv.Atoi(c.Param("id"))
+	delete(users, id)
+	return c.NoContent(http.StatusNoContent)
+}
+
+// fungsi untuk mendapatkan semua user
+func getAllUsers(c echo.Context) error {
+	lock.Lock()
+	defer lock.Unlock()
+	return c.JSON(http.StatusOK, users)
+}
+
+func main() {
+	e := echo.New()
+
+	// middleware yang digunakan
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes dari API
+	e.GET("/users", getAllUsers)
+	e.POST("/users", createUser)
+	e.GET("/users/:id", getUser)
+	e.PUT("/users/:id", updateUser)
+	e.DELETE("/users/:id", deleteUser)
+
+	// memulai server
+	e.Logger.Fatal(e.Start(":1323"))
+}
